cf9e8178180c0e76d556bcc1b0df857e890f1508
vivek magadum <66828419+vivek030@users.noreply.github.com>
2023-02-04 21:51:02 +0530

    Update linuxwg2.sh

--- linuxwg2.sh
+++ linuxwg2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install wireguard-tools
-read -p 'Enter Email ID: ' email
-read -p 'Enter Friendly Name: ' id
-read -p 'Enter API Key: ' api
+read -p 'Enter Email ID: ' email < /dev/tty
+read -p 'Enter Friendly Name: ' id < /dev/tty
+read -p 'Enter API Key: ' api < /dev/tty
 json='{"Email":"'"${email}"'","Identifier":"'"${id}"'"}'
 curl --silent  -f -k -X POST "https://newnew.korplink.com/api/v1/provisioning/peers" -H "accept: text/plain" -H "authorization: Basic $api" -H "Content-Type: application/json" -d $json -o "/etc/wireguard/wg0.conf"
 sudo systemctl start wg-quick@wg0
