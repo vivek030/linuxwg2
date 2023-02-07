7b849a891f16947182c781c3e8ee136d38ba3b1d
vivek magadum <66828419+vivek030@users.noreply.github.com>
2023-02-07 15:13:52 +0530

    Update linuxwg2.sh

--- linuxwg2.sh
+++ linuxwg2.sh
@@ -6,7 +6,7 @@ read -p 'Enter Email ID: ' email < /dev/tty
 read -p 'Enter Friendly Name: ' id < /dev/tty
 read -p 'Enter API Key: ' api < /dev/tty
 json='{"Email":"'"${email}"'","Identifier":"'"${id}"'"}'
-curl --silent  -f -k -X POST "http://167.71.237.148:8080/api/v1/provisioning/peers" -H "accept: text/plain" -H "authorization: Basic $api" -H "Content-Type: application/json" -d $json -o "/etc/wireguard/wg0.conf"
+sudo curl --silent  -f -k -X POST "http://167.71.237.148:8080/api/v1/provisioning/peers" -H "accept: text/plain" -H "authorization: Basic $api" -H "Content-Type: application/json" -d $json -o "/etc/wireguard/wg0.conf"
 sudo systemctl start wg-quick@wg0
 sudo systemctl enable wg-quick@wg0
 resolvectl flush-caches
